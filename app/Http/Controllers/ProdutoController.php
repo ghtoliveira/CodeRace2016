7246bbfc61753301598759a894609ede1ab5e53e
ViniciusMartinsSouza <viniciusmsouza@mx2.unisc.br>
2016-06-11 01:59:40 -0300

    Produto Controller

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -5,8 +5,25 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Produto;
 
 class ProdutoController extends Controller
 {
-    //
+    
+
+
+    public postInserirProduto(Request $request){
+
+    		$produto = new Produto;
+
+    		$produto->nome 			= $request->input('nome');
+    		$produto->valor 		= $request->input('valor');
+    		$produto->descricao 	= $request->input('descricao');
+    		$produto->userId    	= $request->user()->id;
+    		$produto->categoriaId 	= $request->input('categoriaId');
+    		$produto->tagId       	= $request->input('tagId');
+    		$produto->condicaoId   	= $request->input('condicaoId');
+
+    		$produto->save();
+    }
 }
